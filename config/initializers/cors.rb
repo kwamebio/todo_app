8719387fde89e68a03485d6e18338da676286add
benessilfie <bquansah007@gmail.com>
2023-03-05 16:41:48 +0000

    Todo app fix: backend

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,12 +1,12 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors, debug: false, logger: (lambda {
-    Rails.logger
-  }) do
-    allow do
-      origins ['*']
-  
-      resource '/api/*',
-               headers: :any,
-               methods: %i[get post delete put patch]
-    end
+                                                                                          Rails.logger
+                                                                                        }) do
+  allow do
+    origins ['*']
+
+    resource '*',
+             headers: :any,
+             methods: %i[get post delete put patch]
   end
-  
+end
+
